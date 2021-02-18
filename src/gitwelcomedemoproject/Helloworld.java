bef5df0b0d861452804b55cd40957c14ee37af65
Megha Mapalagama <meghamapalagama@meghas-mbp.home>
2021-02-18 09:17:51 -0500

    my scientific project

---
+++ src/gitwelcomedemoproject/Helloworld.java
@@ -0,0 +1,11 @@
+package gitwelcomedemoproject;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Scientis found living creatures under ice in Antarctict");
+
+	}
+
+}
