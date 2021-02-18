05a86c8154c64b0ef0c61eb818f14a8a3bc2f558
Megha Mapalagama <meghamapalagama@meghas-mbp.home>
2021-02-18 09:26:37 -0500

    second commit to a new line

--- src/gitwelcomedemoproject/Helloworld.java
+++ src/gitwelcomedemoproject/Helloworld.java
@@ -5,6 +5,9 @@ public class Helloworld {
 	public static void main(String[] args) {
 		
 		System.out.println("Scientis found living creatures under ice in Antarctict");
+		
+		
+		System.out.println("In Antarctic better to stay away with human intervantion");
 
 	}
 
